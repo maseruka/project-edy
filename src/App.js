90c1d5d7889d9ce4ea52b431fef37652ca540aa3
shonamcray <shonmacray@gmail.com>
2017-09-15 12:38:46 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       <h1>Hello Cryptosavvy</h1>
+       <button className="btn btn-primary">Cryptosavvy</button>
+      </div>
+    );
+  }
+}
+
+export default App;
